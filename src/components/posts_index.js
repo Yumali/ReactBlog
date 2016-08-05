8caa4c55702a69eb1253689a2308f0c9cd4f218e
ArnulfoYumali <asepulveda@yumali.com>
2016-08-05 16:24:01 -0500

    Signed-off-by: ArnulfoYumali <asepulveda@yumali.com>

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux';
+import {fetchPosts} from '../actions/index';
+import {Link} from 'react-router'
 
-export default () => {
-    return <div>List of blog posts.</div>
-};
+class PostsIndex extends Component {
+    
+    componentWillMount(){
+        //console.log('this would be a good time to call an action creator to fetch posts.');
+        this.props.fetchPosts();
+    }
+    
+    render() {
+        return (
+            <div>
+                <div className="text-xs-right">
+                    <Link to="/posts/new" className="btn btn-primary">
+                    Add a Post
+                    </Link>
+                </div>
+                List of blog posts.
+            </div>
+        );
+    }
+}
+
+/*
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({fetchPosts} , dispatch);
+}
+*/
+
+//export default connect(null, mapDispatchToProps)(PostsIndex);
+//export default connect(null, {fetchPosts: fetchPosts})(PostsIndex);
+export default connect(null, {fetchPosts})(PostsIndex);
